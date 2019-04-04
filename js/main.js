423c8a887ff98146341f942be9df684be56f1617
Osman Zakir <osmanzakir90@hotmail.com>
2019-04-04 19:10:29 +0500

    Changed JS variables to const

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // On smaller screens keep only first link and CSS hamburger icon visible in navigation
 // menu and hide the rest of it, until hamburger is clicked which would be when the rest
 // of the navigation menu is shown
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+const mainNav = document.getElementById("js-menu");
+const navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", () => {
   mainNav.classList.toggle("active");
 });
