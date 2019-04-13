a7fb5862e039fec5fcd4c87801ea2fac85adabeb
Osman Zakir <osmanzakir90@hotmail.com>
2019-04-13 21:00:58 +0500

    Took out onclick handler from button on work.html and added event listener to button in JavaScript code with id-selecter

--- js/main.js
+++ js/main.js
@@ -9,16 +9,19 @@ navBarToggle.addEventListener("click", () => {
 
 // Function to dynamically check if currency converter server application is running
 async function isServerAppRunning() {
-  const displayInfo = document.getElementById("display-info");
-  try {
-    const response = await fetch("https://dragonosman.dynu.net:5501/");
-    if (response.status >= 200 && response.status < 300) {
-      displayInfo.textContent =
+  const button = document.getElementById("isrunning");
+  button.addEventListener("click", function() {
+    const displayInfo = document.getElementById("display-info");
+    try {
+      const response = await fetch("https://dragonosman.dynu.net:5501/");
+      if (response.status >= 200 && response.status < 300) {
+        displayInfo.textContent =
         "Server app is running (bold-faced text generated using JavaScript)";
-    }
-  } catch (err) {
-    console.log(err);
-    displayInfo.textContent =
-      "Server app is not running (bold-faced text generated using JavaScript)";
+      }
+     } catch (err) {
+       console.log(err);
+       displayInfo.textContent =
+       "Server app is not running (bold-faced text generated using JavaScript)";
+      }
+    });
   }
-}
