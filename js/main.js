c7e0ecc60dfd9b5f893ba99757e61f02b523c39d
Osman Zakir <osmanzakir90@hotmail.com>
2019-04-13 21:25:39 +0500

    Put onclick handler back in work.html for isServerAppRunning() for the button

--- js/main.js
+++ js/main.js
@@ -9,19 +9,16 @@ navBarToggle.addEventListener("click", () => {
 
 // Function to dynamically check if currency converter server application is running
 async function isServerAppRunning() {
-  let button = document.getElementById("isrunning");
-  button.addEventListener("click", function() {
-    const displayInfo = document.getElementById("display-info");
-    try {
-      const response = await fetch("https://dragonosman.dynu.net:5501/");
-      if (response.status >= 200 && response.status < 300) {
-        displayInfo.textContent =
+  const displayInfo = document.getElementById("display-info");
+  try {
+    const response = await fetch("https://dragonosman.dynu.net:5501/");
+    if (response.status >= 200 && response.status < 300) {
+      displayInfo.textContent =
         "Server app is running (bold-faced text generated using JavaScript)";
-      }
-     } catch (err) {
-       console.log(err);
-       displayInfo.textContent =
-       "Server app is not running (bold-faced text generated using JavaScript)";
-      }
-    });
+    }
+  } catch (err) {
+    console.log(err);
+    displayInfo.textContent =
+      "Server app is not running (bold-faced text generated using JavaScript)";
   }
+}
