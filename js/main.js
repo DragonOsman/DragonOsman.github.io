1668e6bd08271aba05398c113063f026b54af416
Osman Zakir <osmanzakir90@hotmail.com>
2019-04-17 01:12:46 +0500

    Made separate .js file and included it only in head of work.html, for isServerAppRunning function

--- js/main.js
+++ js/main.js
@@ -6,19 +6,3 @@ const navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", () => {
   mainNav.classList.toggle("active");
 });
-
-// Function to dynamically check if currency converter server application is running
-async function isServerAppRunning() {
-  const displayInfo = document.getElementById("display-info");
-  try {
-    const response = await fetch("https://dragonosman.dynu.net:5501/");
-    if (response.status >= 200 && response.status < 300) {
-      displayInfo.textContent =
-        "Server app is running (bold-faced text generated using JavaScript)";
-    }
-  } catch (err) {
-    console.log(err);
-    displayInfo.textContent =
-      "Server app is not running (bold-faced text generated using JavaScript)";
-  }
-}
