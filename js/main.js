65084bbee1d9013cb97766fd135520edc700300e
Osman Zakir <osmanzakir90@hotmail.com>
2019-10-05 23:29:44 +0500

    Made changes to currency converter app description & updated intro on index.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // On smaller screens keep only first link and CSS hamburger icon visible in navigation
 // menu and hide the rest of it, until hamburger is clicked which would be when the rest
 // of the navigation menu is shown
-let mainNav = document.getElementById("js-menu");
+const mainNav = document.getElementById("js-menu");
 const navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", () => {
   mainNav.classList.toggle("active");
